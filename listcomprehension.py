470746dca35f9624a869e3b4c237f751139eed2c
AKHIL T M <akhilthalikulangarakalarikkal@gmail.com>
2025-01-12 22:52:14 +0530

    Update listcomprehension.py

--- listcomprehension.py
+++ listcomprehension.py
@@ -1,4 +1,12 @@
 #+ve input
+N=int(input("Enter the number of elements: "))
+list=[]
+for i in range(list):
+    value=int(input("Enter the value:"))
+    list.append(value)
+positive=[i for i in list if i>0]
+print(positive)
+...............................................
 N=int(input("enter element no:"))
 list=[]
 A="AaEeIiOoUu"
@@ -9,9 +17,10 @@ for i in list:
     if i>0:
         print(i)
 #vowels
-word=input("string:")
-list1=[char for char in word if char in A]
-print(list1)
+word=input("enter the word:")
+V="AaEeIiOoUu"
+list=[char for char in word if char in V]
+print(list)
 #square
 list2=[]
 B=int(input("Enter the no of elements in list:"))
@@ -21,6 +30,10 @@ for i in range(B):
 square=[i*i for i in list2]
 print(square)
 #ordinal value
+list=input("Enter the string: ")
+ordlist=[ord(i) for i in list]
+print(ordlist)
+.......................................
 list3=[]
 string=input("Enter the string")
 for i in string:
